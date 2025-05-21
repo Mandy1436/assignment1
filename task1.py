b29021878ec11504bda966fe976b0cce2b4a13db
Mandy1436 <106649437+Mandy1436@users.noreply.github.com>
2025-05-21 22:52:11 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,29 @@
+#Task 1 ---------------->
+
+#Takes two numbers as input from the user.
+
+num1 = int(input("Enter the first number: "))   
+num2 = int(input("Enter the second number: "))
+
+#for addition
+print("addition: " , num1 + num2)
+
+#for subtraction
+print("subtraction: ", num1 - num2)
+
+#for multiplication
+print("multiplication: ", num1 * num2)
+
+#for division
+print("division: ", num1 / num2)
+
+
+#Task 2 ---------------->
+
+#Create a Personalized Greeting.
+
+first_name = input(str("Enter your first name: " ))
+last_name = input(str("Enter your last name: " ))
+
+# Display the full name and a personalized greeting
+print("Hello,", first_name, last_name, "! Welcome to the python program")
